0189f94ff3de580cd7b5fe5a8039e81d39b32fb9
Olha Izbash <olgaizbash@gmail.com>
2024-10-30 13:08:29 +0100

    todo list done

--- smalltickets/domManipulation/2/2.js
+++ smalltickets/domManipulation/2/2.js
@@ -1,11 +1,13 @@
 const refs = {
   form: document.querySelector(".form-add-todo"),
-  filterAllTasks: document.querySelector(".all-tasks"),
-  filterCompletedTasks: document.querySelector(".completed-tasks"),
-  filterUncompletedTasks: document.querySelector(".uncompleted-tasks"),
+  filter: document.querySelector(".filter-bar"),
   todoList: document.querySelector(".todo-list"),
-  checkbox: document.querySelectorAll(".checkbox-wrapper"),
+  modal: document.querySelector(".backdrop"),
+  closeModal: document.querySelector(".close-modal"),
+  editForm: document.querySelector(".edit-form"),
 };
+
+let filtered = [];
 
 let tasks = localStorage.getItem("tasks")
   ? JSON.parse(localStorage.getItem("tasks"))
@@ -15,7 +17,6 @@ refs.form.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
   const formDataObj = Object.fromEntries(formData);
-  console.log(formDataObj);
   let id = Math.floor(Math.random() * Date.now());
 
   tasks.push({
@@ -25,80 +26,165 @@ refs.form.addEventListener("submit", (e) => {
   });
 
   localStorage.setItem("tasks", JSON.stringify(tasks));
-  if (tasks.length > 0) {
-    let todoItem = createList(tasks);
-    refs.todoList.innerHTML = todoItem;
-  }
-
+  renderList();
   refs.form.reset();
+});
+
+const closeModal = () => {
+  refs.modal.classList.add("is-hidden");
+};
+
+refs.modal.addEventListener("click", (e) => {
+  if (e.currentTarget === e.target) {
+    closeModal();
+  }
+});
+
+refs.closeModal.addEventListener("click", (e) => {
+  if (e.currentTarget === e.target) {
+    closeModal();
+  }
 });
 
 function createList(dataList) {
   return dataList
-    .map(({ todoTask, id }) => {
+    .map(({ todoTask, id, status }) => {
       return `
-          <li class="todo-item" >
-          <button data-${id} class="checkbox-wrapper">
-          <svg class="checkbox-icon icon-unchecked" width="30" height="30" >
-            <use
-              href="../../../assets/sprite.svg#icon-checkbox-unchecked"></use>
-          </svg>
-          <svg class="checkbox-icon is-hidden icon-checked" width="30" height="30" data-${id}>
-            <use
-              href="../../../assets/sprite.svg#icon-checkbox-checked"></use>
-          </svg>
+        <li class="todo-item" data-id="${id}">
+          <button data-id="${id}" class="checkbox-wrapper">
+            <svg class="checkbox-icon icon-unchecked ${
+              status === "completed" ? "is-hidden" : ""
+            }" width="30" height="30">
+              <use href="../../../assets/sprite.svg#icon-checkbox-unchecked"></use>
+            </svg>
+            <svg class="checkbox-icon icon-checked ${
+              status === "completed" ? "" : "is-hidden"
+            }" width="30" height="30">
+              <use href="../../../assets/sprite.svg#icon-checkbox-checked"></use>
+            </svg>
           </button>
-          <p class="todo-text">${todoTask}</p>
+          <p class="todo-text" style="text-decoration: ${
+            status === "completed" ? "line-through" : "none"
+          };">${todoTask}</p>
           <div class="button-wrapper">
-            <button class="edit-btn">Edit</button>
-            <button class="delete-btn">Delete</button>
+            <button class="edit-btn" data-id="${id}">Edit</button>
+            <button class="delete-btn" data-id="${id}">Delete</button>
           </div>
         </li>
-        `;
+      `;
     })
     .join("");
 }
 
+function renderList() {
+  const todoItem = createList(tasks);
+  refs.todoList.innerHTML = todoItem;
+}
+
 function onLoad() {
-  if (!tasks || tasks.length === 0) {
+  if (tasks.length === 0) {
     refs.todoList.innerText = `You haven't had any TO-DO tasks yet`;
   } else {
-    let todoItem = createList(tasks);
-    refs.todoList.innerHTML = todoItem;
+    renderList();
   }
 }
 
-refs.checkbox.forEach((item) =>
-  item.addEventListener("click", (event) => toggleCheckbox(item, event))
-);
-console.log(refs.checkbox);
+refs.todoList.addEventListener("click", (event) => {
+  const checkboxWrapper = event.target.closest(".checkbox-wrapper");
+  const deleteBtn = event.target.closest(".delete-btn");
+  const editBtn = event.target.closest(".edit-btn");
+  if (checkboxWrapper) {
+    toggleCheckbox(checkboxWrapper);
+  }
+  if (deleteBtn) {
+    onDelete(deleteBtn);
+  }
+  if (editBtn) {
+    onEdit(editBtn);
+  }
+});
 
-function toggleCheckbox(item, event) {
-  const idOfCheckbox = item.dataset;
-  const uncheckedIcon = button.querySelector(".icon-unchecked");
-  const checkedIcon = button.querySelector(".icon-checked");
-  const idOfTodo = event.target.closest("li").dataset;
-  const todoSelect = idOfTodo.map((el) => el.id);
-  let isChecked = todoSelect.includes(idOfTodo.id);
+refs.filter.addEventListener("click", (event) => {
+  const filterOption = event.target.closest("button");
+  let elem = null;
+  switch (filterOption.textContent) {
+    case "All":
+      filtered = [...tasks];
+      elem = createList(filtered);
+      refs.todoList.innerHTML = elem;
+      break;
+    case "Completed":
+      filtered = tasks.filter((el) => el.status === "completed");
+      elem = createList(filtered);
+      refs.todoList.innerHTML = elem;
+      break;
+    case "Uncompleted":
+      filtered = tasks.filter((el) => el.status === "uncompleted");
+      elem = createList(filtered);
+      refs.todoList.innerHTML = elem;
+      break;
+  }
+});
 
-  if (!isChecked) {
-    uncheckedIcon.classList.remove("is-hidden");
-    checkedIcon.classList.add("is-hidden");
+function toggleCheckbox(item) {
+  const idOfCheckbox = item.dataset.id;
+  const uncheckedIcon = item.querySelector(".icon-unchecked");
+  const checkedIcon = item.querySelector(".icon-checked");
+  const todoText = item.closest("li").querySelector(".todo-text");
 
-    // itemsInCart.push(idOfItem);
-    // refs.cartCounter.textContent = itemsInCart.length;
-    // save("cart", itemsInCart);
-  } else {
+  tasks = tasks.map((task) => {
+    if (task.id == idOfCheckbox) {
+      task.status = task.status === "completed" ? "uncompleted" : "completed";
+    }
+    return task;
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  if (checkedIcon.classList.contains("is-hidden")) {
     uncheckedIcon.classList.add("is-hidden");
     checkedIcon.classList.remove("is-hidden");
-    const todoText = event.target.closest("p");
     todoText.style.textDecoration = "line-through";
-    // const itemsInCartNew = [];
-    // for (const i of itemsInCart) {
-    //   if (i.id != idOfItem.id) {
-    //     itemsInCartNew.push(i);
-    //   }
-    // }
+  } else {
+    uncheckedIcon.classList.remove("is-hidden");
+    checkedIcon.classList.add("is-hidden");
+    todoText.style.textDecoration = "none";
   }
 }
+
+function onDelete(item) {
+  let idOfTask = item.dataset.id;
+  console.log(idOfTask);
+  tasks = tasks.filter((task) => task.id !== Number(idOfTask));
+  console.log(tasks);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  onLoad();
+}
+
+function onEdit(item) {
+  refs.modal.classList.remove("is-hidden");
+  refs.editForm.dataset.id = item.dataset.id;
+}
+
+function handleEditSubmit(e) {
+  e.preventDefault();
+  const idOfTask = Number(refs.editForm.dataset.id);
+  const formData = new FormData(e.target);
+  const formDataObj = Object.fromEntries(formData);
+
+  tasks = tasks.map((task) => {
+    if (task.id === idOfTask) {
+      task.todoTask = formDataObj.editedTodoTask;
+    }
+    return task;
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  refs.editForm.reset();
+  closeModal();
+  onLoad();
+}
+
+refs.editForm.addEventListener("submit", handleEditSubmit);
+
 onLoad();
