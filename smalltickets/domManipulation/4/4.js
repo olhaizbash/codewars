fefce470e20a89c04b54c4b6d5fc8acf8be8b6da
Olha Izbash <olgaizbash@gmail.com>
2024-11-01 13:29:07 +0100

    blog add functions

--- smalltickets/domManipulation/4/4.js
+++ smalltickets/domManipulation/4/4.js
@@ -4,7 +4,7 @@ const postsList = document.querySelector(".posts-list");
 
 let content = localStorage.getItem("content")
   ? JSON.parse(localStorage.getItem("content"))
-  : [];
+  : [...data];
 
 function onLoad() {
   if (content.length === 0) {
@@ -25,17 +25,83 @@ function createList(dataList) {
       return `
             <li class="post-item" data-id="${post.id}">
             <div class="post-wrapper">
+            <div class="post-info">
                         <p>${post.author}</p>
                         <p>${post.date}</p>
+                        </div>
                         <p>${post.text}</p>
             </div>
               <div class="button-wrapper">
-                <button class="comment-btn" data-id="${post.id}"><span class="comments-amount">${post.comments.length}</span>Comments</button>
+                <button class="comment-btn" data-id="${post.id}"><span class="comments-amount">(${post.comments.length})</span> Comments</button>
+                <button class="reply-btn" data-id="${post.id}">Reply</button>
               </div>
+              <ul class="comments-list" data-id="${post.id}" id="comments-list"></ul>
             </li>
           `;
     })
     .join("");
 }
-localStorage.setItem("content", JSON.stringify(data));
+
+function createComments(comments) {
+  return comments
+    .map((comment) => {
+      return `
+                <li class="comment-item" data-id="${comment.id}">
+                <div class="comment-wrapper">
+                  <div class="comment-info">
+                    <p>${comment.author}</p>
+                    <p>${comment.date}</p>
+                  </div>
+                  <p>${comment.text}</p>
+                </div>
+                <ul class="replies-list" data-id="${comment.id}">
+                  ${createReplies(comment.replies)}
+                </ul>
+                </li>
+              `;
+    })
+    .join("");
+}
+
+function createReplies(replies) {
+  return replies
+    .map((reply) => {
+      return `
+                <li class="reply-item" data-id="${reply.id}">
+                  <div class="reply-wrapper">
+                    <div class="reply-info">
+                      <p>${reply.author}</p>
+                      <p>${reply.date}</p>
+                    </div>
+                    <p>${reply.text}</p>
+                  </div>
+                </li>
+              `;
+    })
+    .join("");
+}
+
+postsList.addEventListener("click", (e) => {
+  const commentsBtn = e.target.closest(".comment-btn");
+  const replyBtn = e.target.closest(".reply-btn");
+  if (commentsBtn) {
+    showComments(commentsBtn);
+  }
+  if (replyBtn) {
+    addComment(replyBtn);
+  }
+});
+
+function showComments(button) {
+  const postId = Number(button.dataset.id);
+  const post = content.find((post) => post.id === postId);
+
+  if (!post || post.comments.length === 0) return;
+
+  const commentsList = document.querySelector(
+    `.comments-list[data-id="${postId}"]`
+  );
+  commentsList.innerHTML = createComments(post.comments);
+}
+
 onLoad();
--- smalltickets/domManipulation/4/data.js
+++ smalltickets/domManipulation/4/data.js
@@ -2,27 +2,27 @@ export const data = [
   {
     id: 1,
     author: "Kevin",
-    date: new Date(),
-    text: "12 interview questions that come up every time (And how to answer them) The difference between good candidates and the best? Preparation.The truth is, the most prepared candidate usually wins. So, if you want to stand out prepare for these 12 questions:",
+    date: new Date().toLocaleDateString(),
+    text: "With Election Day rapidly approaching, anything that can stop you from staring at the latest polls and crying out from dread is a good thing—and for some residents of Hatboro, Pennsylvania (not to mention thousands of out-of-town visitors), the pleasant diversion du jour is following the viral “cookie election” that Lochel’s Bakery has been holding since 2008. As in prior years, Lochel’s frosted, sprinkle-covered cookies have been iced with the names of this year’s candidates—and while the “Trump 2024” cookies are outselling the “Harris 2024” ones so far, owner Kathleen Romano Lochel urges one and all to remember that her cookie election is hardly The New York Times needle.</br></br> “I always like to remind people that this is not scientific and it’s just a cookie,” she tells Vogue. (Nevertheless, it’s worth noting that Lochel’s cookie election reflected the real-life results in 2008, 2012, and 2016. Plus, the bakery’s location, in one of the United States’ most politically significant swing states, adds a little extra heft to the count’s import.)",
     comments: [
       {
         id: 2,
         author: "Michael",
-        date: new Date(),
-        text: "Nice, i like it",
+        date: new Date().toLocaleDateString(),
+        text: "Nice, I like it",
         replies: [],
       },
       {
         id: 3,
         author: "Chao",
-        date: new Date(),
-        text: "Useful tips",
+        date: new Date().toLocaleDateString(),
+        text: "I love cookies",
         replies: [
           {
             id: 4,
             author: "Kevin",
-            date: new Date(),
-            text: "i agree",
+            date: new Date().toLocaleDateString(),
+            text: "I agree",
           },
         ],
       },
